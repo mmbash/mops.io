53689ed63fa007bd9a03ea355dca7664f5f982bd
Mike <mike.michel@mmbash.de>
2014-10-30 19:13:00 +0100

    First Upload

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+
+// BASE SETUP
+// ==============================================
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT_RUNTIME || process.env.PORT || 3000;
+var request = require('request');
+
+app.get('/api/repos', function (req, res) {
+    request('http://192.168.1.188:5000/v1/search', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // from within the callback, write data to response, essentially returning it.
+            parsedBody = JSON.parse(body);
+            console.log(typeof (parsedbody))
+            console.log(body)
+            res.send(parsedBody);
+        }
+    })
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+// ROUTES
+// ==============================================
+
+// sample route with a route the way we're used to seeing it
+app.get('/sample', function (req, res) {
+    res.send('this is a sample!');
+});
+
+// we'll create our routes here
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);

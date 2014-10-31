5371e448ef02ec97282581507ecb3c13dc8ac33c
Mike <mike.michel@mmbash.de>
2014-10-31 15:23:58 +0100

    style

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var port = process.env.PORT_RUNTIME || process.env.PORT || 3000;
 var request = require('request');
 
 app.get('/api/repos', function (req, res) {
-    request('http://192.168.1.188:5000/v1/search', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // from within the callback, write data to response, essentially returning it.
-            parsedBody = JSON.parse(body);
-            console.log(typeof (parsedbody))
-            console.log(body)
-            res.send(parsedBody);
-        }
-    })
+  request('http://localhost:5000/v1/search', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // from within the callback, write data to response, essentially returning it.
+      parsedBody = JSON.parse(body);
+      console.log(typeof (parsedbody))
+      console.log(body)
+      res.send(parsedBody);
+    }
+  })
 });
 
 
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + '/public'));
 
 // sample route with a route the way we're used to seeing it
 app.get('/sample', function (req, res) {
-    res.send('this is a sample!');
+  res.send('this is a sample!');
 });
 
 // we'll create our routes here

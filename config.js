ba52db881e0cf3b080024821ccfa72d1744e264f
Marc Zimmermann <marc.zimmermann@dumontnet.de>
2014-10-31 16:55:55 +0100

    add a config file and a proxy for start,delete,list apps in marathon

---
+++ config.js
@@ -0,0 +1,14 @@
+// Class to define the urls for the rest interface
+// Version v1
+// author: marc.zimmermann@mmbash.de
+
+// Deployment url
+exports.LISTAPPS = '/v1/apps';
+exports.DEPLOYAPPS = '/v1/apps';
+exports.DELETEAPPS = '/v1/apps/:id';  
+
+// Mesoss urls
+exports.MARATHONHOST = 'http://mesosmaster02:8080';
+exports.MARATHONLISTAPPS = '/v2/apps';
+exports.MARATHONPUTAPPS = '/v2/apps';
+exports.MARATHONDELETEAPP = '/v2/apps/';
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT_RUNTIME || process.env.PORT || 3000;
 var request = require('request');
+var config = require('./config.js');
 
 app.get('/api/repos', function (req, res) {
     request('http://192.168.1.188:5000/v1/search', function (error, response, body) {
@@ -15,11 +16,43 @@ app.get('/api/repos', function (req, res) {
             parsedBody = JSON.parse(body);
             console.log(typeof (parsedbody))
             console.log(body)
-            res.send(parsedBody);
+            res.send(parseBody);
         }
     })
 });
 
+// list all running apps
+app.get(config.LISTAPPS,function getApps(req,res) {
+	console.log("Get running apps");
+  try {
+		req.pipe(request.get(config.MARATHONHOST + config.MARATHONLISTAPPS)).pipe(res);
+  }
+  catch (e) {
+		console.log(e);
+  }
+});
+
+// stop a app
+app.delete(config.DELETEAPPS,function deleteApps(req,res) {
+	console.log ("Delete an app");
+  try {
+		req.pipe(request.del(config.MARATHONHOST + config.MARATHONDELETEAPP + req.params.id)).pipe(res);
+  }
+  catch (e) {
+		console.log(e);
+  }
+});
+
+// start a app#
+app.post(config.DEPLOYAPPS,function deployApps(req,res) {
+	console.log ("Start an app");
+  try {
+		req.pipe(request.post(config.MARATHONHOST + config.MARATHONPUTAPPS)).pipe(res);
+  }
+  catch (e) {
+		console.log(e);
+  }
+});
 
 app.use(express.static(__dirname + '/public'));
 

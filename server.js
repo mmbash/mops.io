53364cc797fd5f1d87eaba3b3e353aac0cf72ded
Mike <mike.michel@mmbash.de>
2014-11-01 17:15:02 +0100

    head raus

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ var request = require('request');
 var config = require('./config.js');
 
 app.get('/api/repos', function (req, res) {
-	request('http://10.0.2.15:5000/v1/search', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			// from within the callback, write data to response, essentially returning it.
-			parsedBody = JSON.parse(body);
-			console.log(typeof (parsedbody))
-			console.log(body)
-			res.send(parsedBody);
-		}
-	})
+  request('http://10.0.2.15:5000/v1/search', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // from within the callback, write data to response, essentially returning it.
+      parsedBody = JSON.parse(body);
+      console.log(typeof (parsedbody))
+      console.log(body)
+      res.send(parsedBody);
+    }
+  })
 });
 
-<<<<<<< HEAD
 app.get('/v1/tags', function (req, res) {
   request('http://10.0.2.15:5000/v1/repositories/:name/tags', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -34,53 +33,52 @@ app.get('/v1/tags', function (req, res) {
   })
 });
 
-=======
+=== === =
 // list all running apps
-app.get(config.LISTAPPS,function getApps(req,res) {
-	console.log('Get running apps');
-	req.pipe(request.get(config.MARATHONHOST + config.MARATHONLISTAPPS, function (error,response,body){
-    if (error){
+app.get(config.LISTAPPS, function getApps(req, res) {
+  console.log('Get running apps');
+  req.pipe(request.get(config.MARATHONHOST + config.MARATHONLISTAPPS, function (error, response, body) {
+    if (error) {
       console.error('Connection error: ' + error.code);
     }
   })).pipe(res);
 });
->>>>>>> origin/master
 
 // get infos of a running app
-app.get(config.GETINFOSAPP, function getAppInfo(req,res) {
-	console.log('Get info of app: '+req.params.id);
-	req.pipe(request.get(config.MARATHONHOST + config.MARATHONGETINFOSAPP + req.params.id, function (error,response,body){
-		if (error){
-    	console.error('Connection error: ' + error.code);
+app.get(config.GETINFOSAPP, function getAppInfo(req, res) {
+  console.log('Get info of app: ' + req.params.id);
+  req.pipe(request.get(config.MARATHONHOST + config.MARATHONGETINFOSAPP + req.params.id, function (error, response, body) {
+    if (error) {
+      console.error('Connection error: ' + error.code);
     }
-	})).pipe(res);
+  })).pipe(res);
 });
 
 // stop a app
-app.delete(config.DELETEAPP, function deleteApps(req,res) {
-	console.log ('Delete an app');
-	req.pipe(request.del(config.MARATHONHOST + config.MARATHONDELETEAPP + req.params.id, function (error,response,body){
-    if (error){
+app.delete(config.DELETEAPP, function deleteApps(req, res) {
+  console.log('Delete an app');
+  req.pipe(request.del(config.MARATHONHOST + config.MARATHONDELETEAPP + req.params.id, function (error, response, body) {
+    if (error) {
       console.error('Connection error: ' + error.code);
     }
   })).pipe(res);
 });
 
 // start a app
-app.post(config.DEPLOYAPP, function deployApps(req,res) {
-	console.log ('Start an app');
-	req.pipe(request.post(config.MARATHONHOST + config.MARATHONDEPLOYAPP, function (error,response,body){
-    if (error){
+app.post(config.DEPLOYAPP, function deployApps(req, res) {
+  console.log('Start an app');
+  req.pipe(request.post(config.MARATHONHOST + config.MARATHONDEPLOYAPP, function (error, response, body) {
+    if (error) {
       console.error('Connection error: ' + error.code);
     }
   })).pipe(res);
 });
 
 // change params of a app
-app.put(config.CHANGEAPP,function changeApps(req,res) {
-	console.log ('Change an app: ' + req.params.id);
-	req.pipe(request.put(config.MARATHONHOST + config.MARATHONCHANGEAPP + req.params.id, function (error,response,body){
-    if (error){
+app.put(config.CHANGEAPP, function changeApps(req, res) {
+  console.log('Change an app: ' + req.params.id);
+  req.pipe(request.put(config.MARATHONHOST + config.MARATHONCHANGEAPP + req.params.id, function (error, response, body) {
+    if (error) {
       console.error('Connection error: ' + error.code);
     }
   })).pipe(res);
@@ -88,9 +86,9 @@ app.put(config.CHANGEAPP,function changeApps(req,res) {
 
 // catch uncaught exception
 process.on('uncaughtException', function (err) {
-	console.error('uncaughtException: ' + err.message);
-	console.error(err.stack);
-	process.exit(1);
+  console.error('uncaughtException: ' + err.message);
+  console.error(err.stack);
+  process.exit(1);
 });
 
 app.use(express.static(__dirname + '/public'));

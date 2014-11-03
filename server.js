5fbe80d6a16def588373b4d65cabda6e3119b8eb
Mike <mike.michel@mmbash.de>
2014-11-03 10:34:53 +0100

    df

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.get('/api/repos', function (req, res) {
 // TAGSd
 app.get('/v1/tags', function getApps(req, res) {
   console.log('Get tags');
-  console.log('[' + new Date() + '] ', req.request);
   req.pipe(request.get(config.REGISTRYHOST + config.REGREPOSTAGS, function (error, response, body) {
+    console.log('[' + new Date() + '] ', req.request);
     if (error) {
       console.error('Connection error: ' + error.code);
     }

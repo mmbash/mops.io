3adfbaa1fbb87559e01aee982dafd0591bf4f343
Marc Zimmermann <marc.zimmermann@mmbash.de>
2014-11-18 23:14:03 +0100

    try to add a getMesosMaster function

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var _ = require('underscore');
 var async = require('async');
 var docker = require('dockerode');
 var Mesos = require('./mesos.js');
-var mesos = new Mesos ('http://mesosmaster01:5050');
+var mesos = new Mesos (['http://mesosmaster01:5050']);
 
 db.serialize(function () {
   db.run("CREATE TABLE IF NOT EXISTS settings (marathon TEXT, registry TEXT, id INT)");
@@ -235,31 +235,17 @@ app.get(config.DOCKERLIST, function getDockerContainers(req, res) {
 // get all mesos slaves
 app.get('/v1/getslaves', function getMesosSlaves(req, res) {
   console.log('Get all mesoslaves');
-  var mesos = new Mesos ('http://mesosmaster01:5050');
-  mesos.getAllSlaves(function logSlaves(body) {
+	mesos.getAllSlaves(function logSlaves(body) {
     res.send(body);
 	});	    
-  var target = {};
-  var target1;
-  var target2;
-  var stream2 = req.pipe(request.get(config.DOCKERHOST1 + config.DOCKERLIST, function (error, response, body) {
-    target2 = body;
-  }));
+});
 
-  var stream1 = req.pipe(request.get(config.DOCKERHOST2 + config.DOCKERLIST, function (error, response, body) {
-    target1 = body;
-  }));
-
-  stream1.on('end', function () {
-    console.log("Stream1 end");
-    console.log(target1);
-  });
-
-  stream2.on('end', function () {
-    console.log("Stream2 end");
-    console.log(target2);
-  });
-  /*res.send(target);*/
+// debug function
+app.get('/debug', function getMesosSlaves(req, res) {
+  console.log('Debug');
+	mesos.getActiveMesosMaster(function getMesosMaster(body) {
+    res.send(body);
+	});	    
 });
 
 app.use(express.static(__dirname + '/public'));
